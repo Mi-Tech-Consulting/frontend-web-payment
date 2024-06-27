f19be278521fd1cfdd99f7c72ec18aaadc27615e
yeabsra-leul <74534830+yeabsra-leul@users.noreply.github.com>
2024-06-27 09:59:46 +0300

    FInancial page implementation

---
+++ public/financial_logos/index.ts
@@ -0,0 +1,10 @@
+export { default as AtbLogo } from './atb.png';
+export { default as CibcLogo } from './cibc.png';
+export { default as DesjardinLogo } from './desjardin.png';
+export { default as HsbcLogo } from './hsbc.png';
+export { default as ManulifeLogo } from './manulife.png';
+export { default as MeridianLogo } from './meridian.png';
+export { default as PaypalLogo } from './paypal.png';
+export { default as ScotiaLogo } from './scotia.png';
+export { default as TangerineLogo } from './tangerine.png';
+export { default as TdbankLogo } from './tdbank.png';
